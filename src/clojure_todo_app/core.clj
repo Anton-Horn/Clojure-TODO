2cf4458ce1d44d3dc9068ecffc2a8b07a8f8aa07
Tobias Metzger <contact@tobias-metzger.de>
2024-07-31 19:34:23 +0200

    Initial commit

---
+++ src/clojure_todo_app/core.clj
@@ -0,0 +1,11 @@
+(ns clojure-todo-app.core
+    (:use ring.adapter.jetty))
+
+(defn handler [request]
+      {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body "Hello World"})
+
+(defn -main
+      [& args]
+      (run-jetty handler {:port 3000}))
